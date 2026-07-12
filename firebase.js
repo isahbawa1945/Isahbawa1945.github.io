37cacc3aba1173064db52039499409448f375f7a
bawa1945 <isahbawa1945@gmail.com>
2026-07-13 00:54:03 +0100

    Alhamdulillah

---
+++ firebase.js
@@ -0,0 +1,30 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkqdWH8i6yH5bjYGYVtuZaoLRov-Nmmeg",
+  authDomain: "madarasatul-arkanul-islam.firebaseapp.com",
+  projectId: "madarasatul-arkanul-islam",
+  storageBucket: "madarasatul-arkanul-islam.firebasestorage.app",
+  messagingSenderId: "128596209298",
+  appId: "1:128596209298:web:943f249d732975ebe2404b",
+  measurementId: "G-2TGLVJR4EW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const form = document.getElementById("admissionForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    alert("Firebase is connected successfully!");
+});
